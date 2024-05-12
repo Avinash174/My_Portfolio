8c2f1e43dd1197598574e4da4feac2514c72df27
Avinash174 <avinashmagar15@gmail.com>
2024-05-12 11:53:34 +0530

    new changes done

--- lib/home.dart
+++ lib/home.dart
@@ -10,14 +10,28 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final Shader OverallTextGradiant = const LinearGradient(colors: [
+    Color(0xff1f005c),
+    Color(0xff5b0060),
+    Color(0xff870160),
+    
+    Color(0xfff39060),
+    Color.fromARGB(255, 255, 89, 0)
+  ]).createShader(Rect.fromLTWH(0, 0, 200.0, 70));
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(168, 0, 0, 0),
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
+        // actions: [
+        // Next Version add light dark mode
+        // ],
         leading: PopupMenuButton(
+          color: Colors.black,
           icon: const Icon(
             Icons.menu,
+            color: Colors.white,
           ),
           itemBuilder: (BuildContext context) => [
             PopupMenuItem(
@@ -25,7 +39,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 onPressed: () {},
                 child: Text(
                   'Project',
-                  style: GoogleFonts.workSans(
+                  style: GoogleFonts.lato(
                     color: Colors.white,
                   ),
                 ),
@@ -36,7 +50,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 onPressed: () {},
                 child: Text(
                   'About',
-                  style: GoogleFonts.workSans(
+                  style: GoogleFonts.lato(
                     color: Colors.white,
                   ),
                 ),
@@ -44,23 +58,45 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ],
         ),
+        backgroundColor: Colors.transparent,
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [
-              Color.fromARGB(255, 19, 16, 16),
-              Color.fromARGB(255, 0, 0, 0)
-            ],
-          ),
+      body: SlidingSheet(
+        color: Colors.white,
+        elevation: 8,
+        cornerRadius: 16,
+        snapSpec: const SnapSpec(
+          // Enable snapping. This is true by default.
+          snap: true,
+          // Set custom snapping points.
+          snappings: [0.4, 0.7, 1.0],
+          // Define to what the snappings relate to. In this case,
+          // the total available space that the sheet can expand to.
+          positioning: SnapPositioning.relativeToAvailableSpace,
         ),
-        child: SlidingSheet(
-          color: Colors.white,
-          elevation: 8,
-          cornerRadius: 30,
-        ),
+        builder: (context, state) {
+          return Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Color.fromARGB(255, 19, 16, 16),
+                  Color.fromARGB(255, 0, 0, 0)
+                ],
+              ),
+            ),
+          );
+        },
+        body: Container(
+            height: 500,
+            margin: const EdgeInsets.only(left: 20, right: 20, top: 30),
+            child: const Column(
+              children: [
+                Row(
+                  children: [],
+                )
+              ],
+            )),
       ),
     );
   }
